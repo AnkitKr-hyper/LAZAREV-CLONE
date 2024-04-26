181aad42c37c4a7d418eaa175400319f0ad428bd
Ankit Kumar <ankitkjha1986@gmail.com>
2024-04-26 22:47:53 +0530

    Font Changed

--- script.js
+++ script.js
@@ -63,10 +63,11 @@ function navAnimation() {
     var nav = document.querySelector("nav")
 
     nav.addEventListener("mouseenter", function () {
+        
         let tl = gsap.timeline()
 
         tl.to("#nav-bottom", {
-            height: "21vh",
+            height: "24vh",
             duration: 0.5
         })
         tl.to(".nav-part2 h5", {
@@ -79,9 +80,10 @@ function navAnimation() {
             // duration:0.3,
             stagger: {
                 amount: 0.5
-            }
+            },
         })
     })
+  
     nav.addEventListener("mouseleave", function () {
         let tl = gsap.timeline()
         tl.to(".nav-part2 h5 span", {
